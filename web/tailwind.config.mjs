b62edfdad2e29b9d3ba94c07430df5035ae6fad4
toastycoder0 <silentworker36743@joseluria.com>
2025-01-25 13:05:37 -0600

    feat: enhance Tailwind config with custom colors and fonts for Spotify branding and improved UI consistency

--- web/tailwind.config.mjs
+++ web/tailwind.config.mjs
@@ -2,7 +2,17 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "spotify-purple": "#4100F5",
+        "spotify-green": "#CEF56A",
+        "spotify-white": "#E9E2FF",
+      },
+      fontFamily: {
+        spotifyMixUI: ["SpotifyMixUI", "sans-serif"],
+        spotifyMixVariable: ["SpotifyMixVariable", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
