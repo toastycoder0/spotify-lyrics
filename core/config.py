c0275e1e5d4623a06cb8b99efa2aeede237b13fe
toastycoder0 <silentworker36743@joseluria.com>
2025-01-25 09:56:56 -0600

    feat: enhance Spotify config management by centralizing environment variable checks and improving error handling for better reliability

--- core/config.py
+++ core/config.py
@@ -3,13 +3,26 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-SPOTIFY_CLIENT_ID = os.getenv("SPOTIFY_CLIENT_ID")
-SPOTIFY_CLIENT_SECRET = os.getenv("SPOTIFY_CLIENT_SECRET")
-SPOTIFY_REFRESH_TOKEN = os.getenv("SPOTIFY_REFRESH_TOKEN")
-SPOTIFY_SP_DC = os.getenv("SPOTIFY_SP_DC")
+
+ENV_VARS_KEYS = {
+    'client_id': 'SPOTIFY_CLIENT_ID',
+    'client_secret': 'SPOTIFY_CLIENT_SECRET',
+    'refresh_token': 'SPOTIFY_REFRESH_TOKEN',
+    'sp_dc': 'SPOTIFY_SP_DC'
+}
+
+MANDATORY_ENV_VARS = [ENV_VARS_KEYS[key] for key in ENV_VARS_KEYS]
+
+for enviroment_variable in MANDATORY_ENV_VARS:
+    if enviroment_variable not in os.environ:
+        raise EnvironmentError(
+            "Failed because {} is not set.".format(enviroment_variable)
+        )
+
+SPOTIFY_CLIENT_ID = os.getenv(ENV_VARS_KEYS['client_id'])
+SPOTIFY_CLIENT_SECRET = os.getenv(ENV_VARS_KEYS['client_secret'])
+SPOTIFY_REFRESH_TOKEN = os.getenv(ENV_VARS_KEYS['refresh_token'])
+SPOTIFY_SP_DC = os.getenv(ENV_VARS_KEYS['sp_dc'])
 
 SPOTIFY_APP_PLATFORM = 'WebPlayer'
 SPOTIFY_USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36'
-
-if not all([SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET, SPOTIFY_SP_DC]):
-    raise EnvironmentError("Missing required Spotify environment variables")
