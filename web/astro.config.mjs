a9f524571333faa5a3d2447fa7b6b7c1e358ada1
toastycoder0 <silentworker36743@joseluria.com>
2025-01-25 11:59:20 -0600

    feat: configure Vite server proxy in Astro for API requests, directing '/api' to local server at port 4000

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -1,5 +1,17 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    server: {
+      proxy: {
+        "/api": {
+          target: "http://localhost:4000",
+          changeOrigin: true,
+          secure: false,
+        },
+      },
+    },
+  },
+});
