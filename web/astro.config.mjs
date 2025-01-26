7e3bd78c6fb47f636acfc3c4d17c9ddbce9bf02b
toastycoder0 <silentworker36743@joseluria.com>
2025-01-26 12:16:50 -0600

    feat: add React integration and update TypeScript config for JSX support to enhance the app's component structure and styling capabilities

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -1,7 +1,7 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
@@ -17,5 +17,5 @@ export default defineConfig({
     },
   },
 
-  integrations: [tailwind()],
+  integrations: [tailwind(), react()],
 });
