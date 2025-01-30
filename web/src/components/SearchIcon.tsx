a9c96c5154909429537a3eeedc8031340f837333
toastycoder0 <silentworker36743@joseluria.com>
2025-01-29 18:16:19 -0600

    feat: add SearchIcon component for improved search functionality, providing a reusable SVG icon in the application

---
+++ web/src/components/SearchIcon.tsx
@@ -0,0 +1,22 @@
+const SearchIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={28}
+      height={28}
+      fill="none"
+      strokeWidth={2.5}
+      stroke="currentColor"
+      viewBox="0 0 24 24"
+      className="m-auto"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="m17 17 4 4M3 11a8 8 0 1 0 16 0 8 8 0 0 0-16 0Z"
+      />
+    </svg>
+  );
+};
+
+export { SearchIcon };
