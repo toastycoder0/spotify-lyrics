edff5b1b69fd15317dc4b334bbf43e391ca5721c
toastycoder0 <silentworker36743@joseluria.com>
2025-01-29 18:20:37 -0600

    feat: replace lyrics display logic in TrackDetails with TrackLyrics component for cleaner code and improved maintainability

--- web/src/components/TrackDetails.tsx
+++ web/src/components/TrackDetails.tsx
@@ -1,5 +1,5 @@
 import { TrackInfo } from "@/components/TrackInfo";
-import { convertMsToSeconds } from "@/utils/convertMsToSeconds";
+import { TrackLyrics } from "@/components/TrackLyrics";
 import { useTrack } from "@/hooks/useTrack";
 
 const TrackDetails = () => {
@@ -21,32 +21,7 @@ const TrackDetails = () => {
         isDisabled={!data.lyrics}
       />
 
-      <main className="p-6 grow min-h-72 flex flex-col bg-[#333] text-white text-xl rounded-xl gap-2 md:p-10 xl:gap-4">
-        {data.lyrics ? (
-          <>
-            {data.lyrics.lines.map((line, index) => (
-              <p
-                key={index}
-                className="flex justify-between items-center text-right gap-6"
-              >
-                <span className="font-bold text-gray-400">
-                  {convertMsToSeconds(line.start_time)}
-                </span>
-                <span className="font-bold xl:text-2xl">{line.words}</span>
-              </p>
-            ))}
-
-            <p className="text-sm mt-4 text-right">
-              Lyrics provided by Musixmatch
-            </p>
-          </>
-        ) : (
-          <p className="xl:text-2xl text-center max-w-96 m-auto">
-            Sorry we couldn't find any lyrics for this track. Please try another
-            one.
-          </p>
-        )}
-      </main>
+      <TrackLyrics lyrics={data.lyrics} />
     </div>
   );
 };
