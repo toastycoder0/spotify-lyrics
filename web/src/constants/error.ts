a3d2f11f3860340f2b8c4eed2e4f9f9207fe9bfb
toastycoder0 <silentworker36743@joseluria.com>
2025-01-29 18:08:16 -0600

    feat: add error messages constant for tracking-related errors to improve user feedback in the application

---
+++ web/src/constants/error.ts
@@ -0,0 +1,6 @@
+export const ERRORS = {
+  NO_TRACK_ID_FOUND: "No track ID found in URL",
+  FAILED_TO_FETCH_TRACK_DATA: "Failed to fetch track data",
+  FAILED_TO_PARSE_TRACK_DATA: "Failed to parse track data",
+  UNKNOWN: "An unknown error occurred",
+};
