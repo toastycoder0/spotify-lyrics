284ce1a5af228e95898a542b02ce76a844a7f15c
toastycoder0 <silentworker36743@joseluria.com>
2025-01-29 18:47:23 -0600

    feat: refactor error handling to include error codes and messages for better track data fetch error management

--- web/src/constants/error.ts
+++ web/src/constants/error.ts
@@ -1,6 +1,23 @@
+export interface ErrorWithCode {
+  code: number;
+  message: string;
+}
+
 export const ERRORS = {
-  NO_TRACK_ID_FOUND: "No track ID found in URL",
-  FAILED_TO_FETCH_TRACK_DATA: "Failed to fetch track data",
-  FAILED_TO_PARSE_TRACK_DATA: "Failed to parse track data",
-  UNKNOWN: "An unknown error occurred",
+  NO_TRACK_ID_FOUND: {
+    code: 404,
+    message: "No track ID found in URL",
+  },
+  FAILED_TO_FETCH_TRACK_DATA: {
+    code: 404,
+    message: "Failed to fetch track data",
+  },
+  FAILED_TO_PARSE_TRACK_DATA: {
+    code: 404,
+    message: "Failed to parse track data",
+  },
+  UNKNOWN: {
+    code: 500,
+    message: "An unknown error occurred",
+  },
 };
--- web/src/hooks/useTrack.ts
+++ web/src/hooks/useTrack.ts
@@ -2,10 +2,10 @@ import { useState, useEffect } from "react";
 import { toast } from "sonner";
 import { BASE_SPOTIFY_TRACK_URL } from "@/constants/url";
 import { trackSchema, type TrackSchema } from "@/validations/track";
-import { ERRORS } from "@/constants/error";
+import { ERRORS, type ErrorWithCode } from "@/constants/error";
 
 const useTrack = () => {
-  const [error, seError] = useState("");
+  const [error, seError] = useState<ErrorWithCode | null>(null);
   const [trackData, setTrackData] = useState<TrackSchema | null>(null);
 
   const fetchTrackData = async () => {
@@ -14,7 +14,7 @@ const useTrack = () => {
       const trackId = url.searchParams.get("q");
 
       if (!trackId) {
-        toast.error(ERRORS.NO_TRACK_ID_FOUND);
+        toast.error(ERRORS.NO_TRACK_ID_FOUND.message);
         seError(ERRORS.NO_TRACK_ID_FOUND);
         return;
       }
@@ -26,7 +26,7 @@ const useTrack = () => {
       );
 
       if (!data.ok) {
-        toast.error(ERRORS.FAILED_TO_FETCH_TRACK_DATA);
+        toast.error(ERRORS.FAILED_TO_FETCH_TRACK_DATA.message);
         seError(ERRORS.FAILED_TO_FETCH_TRACK_DATA);
         return;
       }
@@ -36,14 +36,14 @@ const useTrack = () => {
       const track = trackSchema.safeParse({ ...json, url: trackUrl });
 
       if (!track.success) {
-        toast.error(ERRORS.FAILED_TO_PARSE_TRACK_DATA);
+        toast.error(ERRORS.FAILED_TO_PARSE_TRACK_DATA.message);
         seError(ERRORS.FAILED_TO_PARSE_TRACK_DATA);
         return;
       }
 
       setTrackData(track.data);
     } catch (error) {
-      toast.error(ERRORS.UNKNOWN);
+      toast.error(ERRORS.UNKNOWN.message);
       seError(ERRORS.UNKNOWN);
     }
   };
