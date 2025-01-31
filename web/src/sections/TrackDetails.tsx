de36fb8ccef236cfeaaab8ab6616bc965fc52322
toastycoder0 <silentworker36743@joseluria.com>
2025-01-31 13:19:01 -0600

    feat: implement convertToLrc function for exporting lyrics as .lrc files in TrackDetails section

--- web/src/sections/TrackDetails.tsx
+++ web/src/sections/TrackDetails.tsx
@@ -1,11 +1,33 @@
+import type { TrackLyricsSchema } from "@/validations/track";
 import { TrackInfo } from "@/components/TrackInfo";
 import { TrackLyrics } from "@/components/TrackLyrics";
 import { Loading } from "@/sections/Loading";
 import { Error } from "@/sections/Error";
 import { useTrack } from "@/hooks/useTrack";
+import { convertMsToSeconds } from "@/utils/convertMsToSeconds";
 
 const TrackDetails = () => {
   const { data, error } = useTrack();
+
+  const convertToLrc = (lyrics: TrackLyricsSchema, name: string) => {
+    if (!lyrics) {
+      return;
+    }
+
+    const lines = lyrics.lines.map(
+      (line) => `[00:${convertMsToSeconds(line.start_time)}]${line.words}`
+    );
+
+    const lrcLines = lines.join("\n");
+
+    const blob = new Blob([lrcLines], { type: "text/plain" });
+    const url = URL.createObjectURL(blob);
+
+    const a = document.createElement("a");
+    a.href = url;
+    a.download = `${name}.lrc`;
+    a.click();
+  };
 
   if (error) {
     return <Error error={error} />;
@@ -18,6 +40,14 @@ const TrackDetails = () => {
   return (
     <div className="w-full max-w-7xl mx-auto flex flex-col gap-6 md:gap-8 md:flex-row">
       <TrackInfo
+        onDonwload={() =>
+          convertToLrc(
+            data.lyrics,
+            `${data.track_info.name} - ${data.track_info.artists
+              .map((artist) => artist.name)
+              .join(", ")}`
+          )
+        }
         info={data.track_info}
         trackUrl={data.url}
         isDisabled={!data.lyrics}
