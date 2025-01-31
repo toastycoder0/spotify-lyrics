0f6f92ec40a19e2e410ec21211a16b067202a879
toastycoder0 <silentworker36743@joseluria.com>
2025-01-31 13:18:46 -0600

    feat: enhance time formatting to include leading zeroes for minutes in convertMsToSeconds utility function

--- web/src/utils/convertMsToSeconds.ts
+++ web/src/utils/convertMsToSeconds.ts
@@ -2,9 +2,10 @@ export const convertMsToSeconds = (ms: number): string => {
   const seconds = Math.floor(ms / 1000);
   const minutes = Math.floor(seconds / 60);
 
+  const minutesWithLeadingZero = minutes % 60;
   const secondsWithLeadingZero = seconds % 60;
 
-  return `${minutes}:${
+  return `${minutesWithLeadingZero < 10 ? "0" : ""}${minutes}:${
     secondsWithLeadingZero < 10 ? "0" : ""
   }${secondsWithLeadingZero}`;
 };
