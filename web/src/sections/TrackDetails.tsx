5f36f7cdfb3f1d74fbec1027379ae472d4593357
toastycoder0 <silentworker36743@joseluria.com>
2025-02-03 13:10:19 -0600

    fix: update onDownload handler in TrackDetails to use new convertToLrc utility with track info for lyric downloads

--- web/src/sections/TrackDetails.tsx
+++ web/src/sections/TrackDetails.tsx
@@ -1,33 +1,12 @@
-import type { TrackLyricsSchema } from "@/validations/track";
 import { TrackInfo } from "@/components/TrackInfo";
 import { TrackLyrics } from "@/components/TrackLyrics";
 import { Loading } from "@/sections/Loading";
 import { Error } from "@/sections/Error";
 import { useTrack } from "@/hooks/useTrack";
-import { convertMsToSeconds } from "@/utils/convertMsToSeconds";
+import { convertToLrc } from "@/utils/convertTolrc";
 
 const TrackDetails = () => {
   const { data, error } = useTrack();
-
-  const convertToLrc = (lyrics: TrackLyricsSchema, name: string) => {
-    if (!lyrics) {
-      return;
-    }
-
-    const lines = lyrics.lines.map(
-      (line) => `[00:${convertMsToSeconds(line.start_time)}]${line.words}`
-    );
-
-    const lrcLines = lines.join("\n");
-
-    const blob = new Blob([lrcLines], { type: "text/plain" });
-    const url = URL.createObjectURL(blob);
-
-    const a = document.createElement("a");
-    a.href = url;
-    a.download = `${name}.lrc`;
-    a.click();
-  };
 
   if (error) {
     return <Error error={error} />;
@@ -40,14 +19,7 @@ const TrackDetails = () => {
   return (
     <div className="w-full max-w-7xl mx-auto flex flex-col gap-6 md:gap-8 md:flex-row">
       <TrackInfo
-        onDonwload={() =>
-          convertToLrc(
-            data.lyrics,
-            `${data.track_info.name} - ${data.track_info.artists
-              .map((artist) => artist.name)
-              .join(", ")}`
-          )
-        }
+        onDonwload={() => convertToLrc(data.lyrics, data.track_info)}
         info={data.track_info}
         trackUrl={data.url}
         isDisabled={!data.lyrics}
